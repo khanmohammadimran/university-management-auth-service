41bccf5eb15ac097ef72854f70a07ef3c8b406aa
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2023-05-31 02:08:26 +0600

    check Husky is working or not

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,18 @@
-import express, { Request, Response } from "express";
-import { Application } from "express";
-import cors from "cors";
+import express, { Request, Response } from 'express'
+import { Application } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
 // Parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-const port = 5000;
+      app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Testing
-app.get("/", (req: Request, res: Response) => {
-  res.send("Look mama! I'm creating a mid level project using TypeScript");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send("Look mama! I'm creating a mid level project using TypeScript")
+})
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function tailwind() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log(`✔ Database is connected successfully`);
+    await mongoose.connect(config.database_url as string)
+    console.log(`✔ Database is connected successfully`)
 
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
-    });
+      console.log(`Application listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log(`Failed to connect database`, err);
+    console.log(`Failed to connect database`, err)
   }
 }
 
-tailwind();
+tailwind()
