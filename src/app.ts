6d9bf6cc0cbd9ebed5b8f2d79491fd8a83e2bb3f
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2023-05-31 01:12:54 +0600

    [Done] Setup mongoose, cors, parser

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import { Application } from "express";
+import cors from "cors";
+
+const app: Application = express();
+
+app.use(cors());
+
+// Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = 5000;
+
+// Testing
+app.get("/", (req: Request, res: Response) => {
+  res.send("Look mama! I'm creating a mid level project using TypeScript");
+});
+
+export default app;
